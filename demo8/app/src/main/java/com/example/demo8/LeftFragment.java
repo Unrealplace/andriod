5dc0981a24dbe96d01967699b4565d708ead8b91
李杨 <oliverleemail@gmail.com>
2019-12-09 15:03:52 +0800

    Revert "1.fragment 的生命周期"
    This reverts commit 0e78ebb75c972e0fd1a56593bd6c50a2ad126572.

--- demo8/app/src/main/java/com/example/demo8/LeftFragment.java
+++ demo8/app/src/main/java/com/example/demo8/LeftFragment.java
@@ -1,90 +1,21 @@
 package com.example.demo8;
 
-import android.app.Activity;
-import android.content.Context;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-// 一共有运行状态、暂停 状态、停止状态和销毁状态这四种
-
 public class LeftFragment extends Fragment {
-
-    public static final String TAG = "LeftFragment";
-
-    // 当碎片和活动建立关联的时候调用。
-    @Override
-    public void onAttach(@NonNull Context context) {
-        super.onAttach(context);
-        Log.d(TAG,"onAttanch");
-    }
-
-    // 为碎片创建视图(加载布局)时调用
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-
-        Log.d(TAG,"onCreateView");
 
         View view = inflater.inflate(R.layout.left_fragment,container,false);
 
         return view;
     }
-
-    // 确保与碎片相关联的活动一定已经创建完毕的时候调用
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        Log.d(TAG,"onActivityCreated");
-        super.onActivityCreated(savedInstanceState);
-    }
-
-    @Override
-    public void onStart() {
-        Log.d(TAG,"onStart");
-        super.onStart();
-    }
-
-    @Override
-    public void onResume() {
-        Log.d(TAG,"onResume");
-        super.onResume();
-    }
-
-    @Override
-    public void onPause() {
-        Log.d(TAG,"onPause");
-        super.onPause();
-    }
-
-    // 当与碎片关联的视图被移除的时候调用
-    @Override
-    public void onDestroyView() {
-        Log.d(TAG,"onDestroyView");
-        super.onDestroyView();
-    }
-
-    //  当碎片和活动解除关联的时候调用
-    @Override
-    public void onDetach() {
-        Log.d(TAG,"onDetach");
-        super.onDetach();
-    }
-
-    public void updateFragmentFromActivity(String name){
-        if (name.length() > 3) {
-            Toast.makeText(getContext(),name,Toast.LENGTH_SHORT).show();
-        }
-
-        // fragment 中调用 activity 的方法
-        MainActivity activity = (MainActivity) getActivity();
-        activity.updateActivityFromFragment("update message from fragment");
-    }
-
 }
--- demo8/app/src/main/java/com/example/demo8/MainActivity.java
+++ demo8/app/src/main/java/com/example/demo8/MainActivity.java
@@ -1,51 +1,14 @@
 package com.example.demo8;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button btn = findViewById(R.id.left_btn);
-        btn.setOnClickListener(this);
-
-        // 在活动中调用 fragment 的 方法
-        LeftFragment  leftFragment = (LeftFragment) getSupportFragmentManager().findFragmentById(R.id.left_fragment);
-        leftFragment.updateFragmentFromActivity("hello world");
-    }
-
-    public void updateActivityFromFragment(String updateMessage) {
-        Toast.makeText(this,updateMessage,Toast.LENGTH_LONG).show();
-    }
-
-    @Override
-    public void onClick(View v) {
-
-        int vId = v.getId();
-        switch (vId) {
-            case R.id.left_btn:
-
-                // 切换一个fragment ， 并加入返回栈中
-                AnotherFragment anotherFragment = new AnotherFragment();
-                FragmentManager fragmentManager = getSupportFragmentManager();
-                FragmentTransaction transaction = fragmentManager.beginTransaction();
-                transaction.replace(R.id.right_layout,anotherFragment);
-                transaction.addToBackStack(null);
-                transaction.commit();
-                break;
-                default:
-                    break;
-        }
     }
 }
